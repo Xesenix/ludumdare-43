15959bfb365dea6005adff05040e3e2c12210714
Paweł Kapalla <pkapalla@xesenix.pl>
2019-07-10 04:19:39 +0200

    refactor(test): simplify adding libraries to tests

--- src/lib/main.test.ts
+++ src/lib/main.test.ts
@@ -5,28 +5,18 @@ import 'reflect-metadata';
 Enzyme.configure({ adapter: new Adapter() });
 
 const excludeRegexp: RegExp = /.*\/(main|index|interfaces)\./;
+
+const filter = (contextLib) => contextLib
+	.keys()
+	.filter((p: string) => !excludeRegexp.test(p))
+	.forEach(contextLib);
+
 /**
  * We need to load all test files to be included in karma. And all others to generate test coverage.
  * @see https://github.com/webpack-contrib/karma-webpack#alternative-usage
  */
-{
-	const contextLib = (require as any).context('./sound', true, /\.(t|j)sx?$/);
-	contextLib
-		.keys()
-		.filter((p: string) => !excludeRegexp.test(p))
-		.forEach(contextLib);
-}
-{
-	const contextLib = (require as any).context('./sound-scape', true, /\.(t|j)sx?$/);
-	contextLib
-		.keys()
-		.filter((p: string) => !excludeRegexp.test(p))
-		.forEach(contextLib);
-}
-{
-	const contextLib = (require as any).context('./i18n', true, /\.(t|j)sx?$/);
-	contextLib
-		.keys()
-		.filter((p: string) => !excludeRegexp.test(p))
-		.forEach(contextLib);
-}
+filter((require as any).context('./di', true, /\.(t|j)sx?$/));
+filter((require as any).context('./sound', true, /\.(t|j)sx?$/));
+filter((require as any).context('./sound-scape', true, /\.(t|j)sx?$/));
+filter((require as any).context('./i18n', true, /\.(t|j)sx?$/));
+
