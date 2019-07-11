aa8179f6146d54f2d9e5f2b8116d1f9c7f60142b
Paweł Kapalla <pkapalla@xesenix.pl>
2019-07-11 02:43:21 +0200

    refactor(sound): update module structure

--- src/lib/sound/sound.module.ts
+++ src/lib/sound/sound.module.ts
@@ -10,17 +10,9 @@ import { lazyPhaserAudioLoaderServiceProvider } from './phaser/phaser-audio-load
 import { lazyPhaserAudioManagerPluginProvider } from './phaser/phaser-audio-manager-plugin.provider';
 
 export class SoundModule {
-	public static register(app: IApplication) {
-		app.bind<SoundModule>('sound:module').toConstantValue(new SoundModule(app));
-	}
-
-	constructor(
-		// prettier-ignore
-		private app: IApplication,
-		phaser: boolean = true,
-	) {
-		// we dont want to provide AudioContext just as value because we want to wait for it being needed
-		this.app.bind<IAudioContextFactory>('audio-context:factory').toFactory(({ container }: interfaces.Context) => {
+	public static register(app: IApplication, phaser: boolean = true) {
+		// we don't want to provide AudioContext just as value because we want to wait for it being needed
+		app.bind<IAudioContextFactory>('audio-context:factory').toFactory(({ container }: interfaces.Context) => {
 			if (!container.isBound('audio-context')) {
 				container.bind('audio-context').toConstantValue(new AudioContext());
 			}
@@ -29,34 +21,28 @@ export class SoundModule {
 
 		if (phaser) {
 			// TODO: this factory returns class figure out how to correctly type this binding
-			this.app.bind('audio-manager-plugin:provider').toProvider(lazyPhaserAudioManagerPluginProvider);
-			this.app.bind<IAudioFileLoaderProvider>('audio-loader:provider').toProvider(lazyPhaserAudioLoaderServiceProvider);
+			app.bind('audio-manager-plugin:provider').toProvider(lazyPhaserAudioManagerPluginProvider);
+			app.bind<IAudioFileLoaderProvider>('audio-loader:provider').toProvider(lazyPhaserAudioLoaderServiceProvider);
 		} else {
-			this.app.bind<IAudioFileLoaderProvider>('audio-loader:provider').toProvider(audioLoaderProvider);
+			app.bind<IAudioFileLoaderProvider>('audio-loader:provider').toProvider(audioLoaderProvider);
 		}
 
-		this.app
-			.bind<AudioBufferRepository>('audio-repository')
+		app.bind<AudioBufferRepository>('audio-repository')
 			.to(AudioBufferRepository)
 			.inSingletonScope();
-		this.app
-			.bind<IAudioTrack>('audio-mixer:track:master')
+		app.bind<IAudioTrack>('audio-mixer:track:master')
 			.to(AudioMixerTrack)
 			.inSingletonScope();
-		this.app
-			.bind<IAudioTrack>('audio-mixer:track:effects')
+		app.bind<IAudioTrack>('audio-mixer:track:effects')
 			.to(AudioMixerTrack)
 			.inSingletonScope();
-		this.app
-			.bind<IAudioTrack>('audio-mixer:track:music')
+		app.bind<IAudioTrack>('audio-mixer:track:music')
 			.to(AudioMixerTrack)
 			.inSingletonScope();
-		this.app
-			.bind<IAudioTrack>('audio-mixer:track:dialog')
+		app.bind<IAudioTrack>('audio-mixer:track:dialog')
 			.to(AudioMixerTrack)
 			.inSingletonScope();
-		this.app
-			.bind<IAudioMixer>('audio-mixer')
+		app.bind<IAudioMixer>('audio-mixer')
 			.to(AudioMixer)
 			.inSingletonScope();
 	}
