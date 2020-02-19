842d713076ed9c3c87d468335fad9397777c1fe0
Paweł Kapalla <pkapalla@xesenix.pl>
2020-02-20 00:59:07 +0100

    refactor(preloader): encapsulate applications into separate functions

--- game/main.ts
+++ game/main.ts
@@ -1,9 +1,40 @@
-import { IEventEmitter } from 'lib/interfaces';
+import { IApplication, IEventEmitter } from 'lib/interfaces';
 import { IPreloadProgress, preload } from 'lib/preload';
 import ServiceWorkerModule from 'lib/service-worker/service-worker.module';
 
 import IEComponent from './app/ie';
 import PreloadComponent from './app/preloader';
+
+function runNotSupportedBrowserApp(root: HTMLElement) {
+	root.innerHTML = '';
+	root.appendChild(IEComponent());
+}
+
+async function runStyledPreloadApp({
+	root,
+	loadApp,
+	progressFormatter = (loaded: number, total: number) => total > 0 ? `${(100 * loaded / total).toFixed(0)}%` : `${loaded}`,
+	labelFormatter = (label: string) => label.replace(/(\.[a-z0-9]*)/, ''),
+}: {
+	root: HTMLElement;
+	loadApp: () => Promise<new (root: HTMLElement, document: Document, window: Window) => IApplication>,
+	progressFormatter?: (loaded: number, total: number) => string;
+	labelFormatter?: (label: string) => string;
+}) {
+	const renderProgress = (progress: IPreloadProgress) => {
+		root.innerHTML = '';
+		root.appendChild(PreloadComponent(progress, progressFormatter, labelFormatter));
+	};
+	const clearPreload = preload(renderProgress, document);
+
+	const AppModule = await loadApp();
+
+	const app: IApplication = new AppModule(root, document, window);
+	app.get<IEventEmitter>('event-manager').on('app:boot', clearPreload);
+
+	// loading other application modules
+	return app.run();
+}
 
 window.addEventListener('load', () => {
 	const root: HTMLElement = document.getElementById('app') || document.body;
@@ -12,24 +43,14 @@ window.addEventListener('load', () => {
 	const newIE = window.navigator.userAgent.indexOf('Trident/') >= 0;
 
 	if (oldIE || newIE) {
-		root.innerHTML = '';
-		root.appendChild(IEComponent());
+		runNotSupportedBrowserApp(root);
 	} else {
-		const progressFormatter = (loaded: number, total: number) => total > 0 ? `${(100 * loaded / total).toFixed(0)}%` : `${loaded}`;
-		const labelFormatter = (label: string) => label.replace(/(\.[a-z0-9]*)/, '');
-		const clearPreload = preload((progress: IPreloadProgress) => {
-			root.innerHTML = '';
-			root.appendChild(PreloadComponent(progress, progressFormatter, labelFormatter));
-		}, document);
-
-		ServiceWorkerModule.run(window, console)
-			.then(() => import(/* webpackChunkName: "app" */ './app/app.module'))
-			.then(({ AppModule }) => {
-				const app = new AppModule(root, document, window);
-				app.get<IEventEmitter>('event-manager').on('app:boot', clearPreload);
-
-				// loading other application modules
-				return app.start();
-			});
+		runStyledPreloadApp({
+			root,
+			loadApp: async () => {
+				await ServiceWorkerModule.run(window, console);
+				return (await import(/* webpackChunkName: "app" */ './app/app.module') as any).default;
+			},
+		});
 	}
 });
